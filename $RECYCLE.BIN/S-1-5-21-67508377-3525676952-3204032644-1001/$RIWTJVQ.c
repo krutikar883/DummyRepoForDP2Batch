4a4fa6cad80abece8003b7abc5a74d348d1afb22
MRUNAL <MRUNAL@DESKTOP-9HFF3QG>
2023-05-07 23:13:55 +0530

    first commit

---
+++ $RECYCLE.BIN/S-1-5-21-67508377-3525676952-3204032644-1001/$RIWTJVQ.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,mul;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	a=mul*10;
+	b=mul;
+    mul=a*b;
+	printf("enter multiplication of firstDigit and lastDigit is %d",mul);
+	getch();
+	
+}
+
---
+++ TestDemo/src/DemoProject.java
@@ -0,0 +1,9 @@
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
